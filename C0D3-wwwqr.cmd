19f48f9907accbf6a60fe4b7d905b16a3c1cb500
wwwqr <83577933+wwwqr-000@users.noreply.github.com>
2021-06-29 21:10:20 +0200

    Update C0D3-wwwqr.cmd

--- C0D3-wwwqr.cmd
+++ C0D3-wwwqr.cmd
@@ -1,7 +1,7 @@
 @echo off
 color a
 cls
-powershell -command "Invoke-WebRequest -Uri 'https://cdn-116.anonfiles.com/33X4H23fue/77074de0-1624991664/Better%20Minecraft%20[WWWQR]-v17.zip' -OutFile Better-Minecraft-WWWQR-v17.zip"
+powershell -command "Invoke-WebRequest -Uri 'https://cdn-114.anonfiles.com/33X4H23fue/1059e61b-1624994400/Better%20Minecraft%20[WWWQR]-v17.zip' -OutFile Better-Minecraft-WWWQR-v17.zip"
 powershell -command "Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/wwwqr-000/wwwqr-SMP.exe/main/Resources/info.txt' -OutFile info.txt"
 powershell -command "Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/wwwqr-000/wwwqr-SMP.exe/main/Resources/Mod-List.txt' -OutFile Mod-List.txt"
 exit
