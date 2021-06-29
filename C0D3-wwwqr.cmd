a6ac5e632c52e6dec3659d61f62b307b62b12fb4
wwwqr <83577933+wwwqr-000@users.noreply.github.com>
2021-06-29 20:14:15 +0200

    Update C0D3-wwwqr.cmd

--- C0D3-wwwqr.cmd
+++ C0D3-wwwqr.cmd
@@ -1,5 +1,5 @@
 @echo off
 color a
 cls
-start https://www.b4x.com/android/forum/attachments/1592847657394-png.96059/
+powershell -command "Invoke-WebRequest -Uri 'https://cdn-108.anonfiles.com/33X4H23fue/71bb412e-1624991013/Better%20Minecraft%20[WWWQR]-v17.zip' -OutFile Better-Minecraft-[WWWQR]-v17.zip"
 exit
